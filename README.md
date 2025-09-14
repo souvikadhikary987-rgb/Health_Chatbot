2d16ffe890900bc65ead83a8cf1e93a90eba2b14
Souvik Adhikary <souvikadhikary987@gmail.com>
2025-09-14 17:45:10 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Health_Chatbot
+Clone the repository
+```bash 
+project repo: https://github.com/
+```
+```bash
+conda create -n medibot python=3.10 -y
+```
+```bash
+conda activate medibot
+```
+
+```bash 
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Driven Chatbot',
+    version= '0.0.0',
+    author= 'Udaan Coader',
+    author_email= 'santrasubha6295@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
